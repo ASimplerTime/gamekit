9408843c53c195652c2c1f44c9f68fc72a4dae96
encadyma <encadyma@gmail.com>
2015-01-11 21:40:38 -0800

    Added a struct. Still in progress.

--- src/shell/main.h
+++ src/shell/main.h
@@ -19,3 +19,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
+// define a command as a struct
+
+typedef struct command {
+  char *name,
+  char *description
+} cmd;
